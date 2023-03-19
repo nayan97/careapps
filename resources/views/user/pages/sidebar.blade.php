13bd1480c597c8d3f9a31157c84657ea9abf152c
nayan97 <nayanislam085@gmail.com>
2023-03-19 23:37:06 +0600

    in sidebar added tag and category

---
+++ resources/views/user/pages/sidebar.blade.php
@@ -0,0 +1,88 @@
+
+<div class="col-lg-4">
+          <div class="sidebar">
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Search</h3>
+              <form action="#" class="search-form">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Type a keyword and hit enter">
+                  <button type="submit" class="btn"><span class="icon mai-search"></span></button>
+                </div>
+              </form>
+            </div>
+            @php
+                $cat = App\Models\Postcategory::all();
+            @endphp
+
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Categories</h3>
+              <ul class="categories">
+                @foreach ($cat as $catss )
+                <li><a href="#">{{ $catss -> name}} <span>12</span></a></li>
+                @endforeach
+               
+              </ul>
+            </div>
+
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Recent Blog</h3>
+              <div class="blog-item">
+                <a class="post-thumb" href="">
+                  <img src="../assets/img/blog/blog_1.jpg" alt="">
+                </a>
+                <div class="content">
+                  <h5 class="post-title"><a href="#">Even the all-powerful Pointing has no control</a></h5>
+                  <div class="meta">
+                    <a href="#"><span class="mai-calendar"></span> July 12, 2018</a>
+                    <a href="#"><span class="mai-person"></span> Admin</a>
+                    <a href="#"><span class="mai-chatbubbles"></span> 19</a>
+                  </div>
+                </div>
+              </div>
+              <div class="blog-item">
+                <a class="post-thumb" href="">
+                  <img src="../assets/img/blog/blog_2.jpg" alt="">
+                </a>
+                <div class="content">
+                  <h5 class="post-title"><a href="#">Even the all-powerful Pointing has no control</a></h5>
+                  <div class="meta">
+                    <a href="#"><span class="mai-calendar"></span> July 12, 2018</a>
+                    <a href="#"><span class="mai-person"></span> Admin</a>
+                    <a href="#"><span class="mai-chatbubbles"></span> 19</a>
+                  </div>
+                </div>
+              </div>
+              <div class="blog-item">
+                <a class="post-thumb" href="">
+                  <img src="../assets/img/blog/blog_3.jpg" alt="">
+                </a>
+                <div class="content">
+                  <h5 class="post-title"><a href="#">Even the all-powerful Pointing has no control</a></h5>
+                  <div class="meta">
+                    <a href="#"><span class="mai-calendar"></span> July 12, 2018</a>
+                    <a href="#"><span class="mai-person"></span> Admin</a>
+                    <a href="#"><span class="mai-chatbubbles"></span> 19</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            @php
+                $tag = App\Models\Posttag::all();
+            @endphp
+
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Tag Cloud</h3>
+              <div class="tagcloud">
+                @foreach ( $tag as $tags )
+                <a href="#" class="tag-cloud-link">{{ $tags -> name}}</a>
+                @endforeach
+                
+            </div>
+
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Paragraph</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur similique, inventore eos fugit cupiditate numquam!</p>
+            </div>
+          </div>
+        </div> 
